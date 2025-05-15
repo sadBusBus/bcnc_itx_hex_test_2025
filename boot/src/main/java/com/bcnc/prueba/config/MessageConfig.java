b4d834cce81afd3a49e2111160aa641b7b02b98e
BusBus <guesanjo.ibkr@gmail.com>
2025-05-15 11:28:36 +0200

    feat: BETTER messages for latest spring boot version

--- boot/src/main/java/com/bcnc/prueba/config/MessageConfig.java
+++ boot/src/main/java/com/bcnc/prueba/config/MessageConfig.java
@@ -3,7 +3,7 @@ package com.bcnc.prueba.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -11,14 +11,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MessageConfig implements WebMvcConfigurer {
 
     @Bean
-    public ResourceBundleMessageSource messageSource() {
-        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-        //messageSource.setBasename("classpath:messages/messages");
-        messageSource.setBasename("messages/messages");
+    public ReloadableResourceBundleMessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages/messages");
         messageSource.setDefaultEncoding("UTF-8");
         messageSource.setFallbackToSystemLocale(true);
         messageSource.setAlwaysUseMessageFormat(true);
-        messageSource.setCacheSeconds(3600);
+        messageSource.setCacheSeconds(5);
         return messageSource;
     }
 }
--- driving/adapter-rest/src/main/java/com/bcnc/prueba/model/ErrorHandler.java
+++ driving/adapter-rest/src/main/java/com/bcnc/prueba/model/ErrorHandler.java
@@ -1,13 +1,20 @@
 package com.bcnc.prueba.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
+import lombok.Data;
 
 import java.util.List;
 
+@Data
 @Builder
 public class ErrorHandler {
+    @JsonProperty("code")
     private String code;
+    @JsonProperty("locale")
     private String locale;
+    @JsonProperty("description")
     private String description;
+    @JsonProperty("details")
     private List<String> details;
 }
