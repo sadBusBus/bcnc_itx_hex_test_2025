486d6499135b9046ffa0ef4483857da7b4e2a87e
BusBus <guesanjo.ibkr@gmail.com>
2025-05-15 15:44:05 +0200

    feat: jackson formatter
          - Better format for dates

--- driving/adapter-rest/src/main/java/com/bcnc/prueba/application/adapter/api/PriceAPI.java
+++ driving/adapter-rest/src/main/java/com/bcnc/prueba/application/adapter/api/PriceAPI.java
@@ -3,6 +3,7 @@ package com.bcnc.prueba.application.adapter.api;
 import com.bcnc.prueba.application.model.price.PriceDTO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,6 @@ public interface PriceAPI {
 
     @Operation(summary = "Te saluda.")
     @GetMapping("/price")
-    ResponseEntity<PriceDTO> getPrice(@RequestParam OffsetDateTime dateTime, @RequestParam Long productId, @RequestParam Long brandId);
+    ResponseEntity<PriceDTO> getPrice(@RequestParam  @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)  OffsetDateTime dateTime, @RequestParam Long productId, @RequestParam Long brandId);
 
 }
--- driving/adapter-rest/src/main/java/com/bcnc/prueba/application/model/price/PriceDTO.java
+++ driving/adapter-rest/src/main/java/com/bcnc/prueba/application/model/price/PriceDTO.java
@@ -1,5 +1,6 @@
 package com.bcnc.prueba.application.model.price;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,7 +16,9 @@ public class PriceDTO {
     private Long productId;
     private Long brandId;
     private Long priceList;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ssXXX")
     private OffsetDateTime startDate;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ssXXX")
     private OffsetDateTime endDate;
     private String price;
 }
